b6f191c90cfa6b751a6e99eb16aff8b72dc93450
unknown <seungjae_lee@kolon.com>
2020-02-21 17:50:49 +0900

    나머지 추가했음...집에서확인좀..

--- myInvestData_KRX산업종목별-Total.R
+++ myInvestData_KRX산업종목별-Total.R
@@ -6,6 +6,8 @@ library(httr)
 library(rvest)
 library(stringr)
 library(readr)
+library(stringr)
+
 
 # 최근 영업일 구하기
 url = 'https://finance.naver.com/sise/sise_deposit.nhn'
@@ -21,6 +23,7 @@ biz_day = GET(url) %>%
 # 산업별 현황 OTP 발급
 gen_otp_url =
   'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+
 gen_otp_data = list(
   name = 'fileDown',
   filetype = 'csv',
@@ -29,12 +32,14 @@ gen_otp_data = list(
   date = biz_day, # 최근영업일로 변경
   lang = 'ko',
   pagePath = '/contents/MKD/03/0303/03030103/MKD03030103.jsp')
+
 otp = POST(gen_otp_url, query = gen_otp_data) %>%
   read_html() %>%
   html_text()
 
 # 산업별 현황 데이터 다운로드
 down_url = 'http://file.krx.co.kr/download.jspx'
+
 down_sector = POST(down_url, query = list(code = otp),
                    add_headers(referer = gen_otp_url)) %>%
   read_html() %>%
@@ -47,6 +52,7 @@ write.csv(down_sector, 'data/krx_sector.csv')
 # 개별종목 지표 OTP 발급
 gen_otp_url =
   'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+
 gen_otp_data = list(
   name = 'fileDown',
   filetype = 'csv',
@@ -71,8 +77,41 @@ down_ind = POST(down_url, query = list(code = otp),
 # 데이터 저장
 write.csv(down_ind, 'data/krx_ind.csv')
 
-# this is end of line
-# 여기까지 확인 요망
+#다운로드된 데이터 정리
+#중복항목 찾고 데이터 쪼인
 
-#RStudio >> git >> Push버튼 >> 업로드 됨. 
+#각 테이블의 공통항목 찾기 >> intersect(names(down_sector), names(down_ind))
+#공통 항목중 공통적으로 없는  데이터 찾기 >> setdiff(down_sector[, '종목명'], down_ind[ ,'종목명'])
 
+
+#merge() 함수는 by를 기준으로 두 데이터를 하나로 합치며, 
+#공통으로 존재하는 종목코드, 종목명을 기준으로 입력해줍니다. 
+#또한 all 값을 TRUE로 설정하면 합집합을 반 환하고, 
+#FALSE로 설정하면 교집합을 반환합니다. 
+#공통으로 존재하는 항목을 원하므로 여기서는 FALSE를 입력합니다
+
+KOR_ticker = merge(down_sector, down_ind,
+                   by = intersect(names(down_sector),
+                                  names(down_ind)),
+                   all = FALSE)
+
+
+#결과확인 테이블-정렬후 보기
+
+KOR_ticker = KOR_ticker[order(-KOR_ticker['시가총액.원.']), ]
+print(head(KOR_ticker))
+
+#스팩과 우선주제거
+#grepl() 함수를 통해 종목명에 ‘스팩’이 들어가는 종목을 찾고, 
+#stringr 패키지의 str_sub() 함수를 통해 종목코드 끝이 0이 아닌 우선주 종목을
+#찾을 수 있습니다.
+
+KOR_ticker[grepl('스팩', KOR_ticker[, '종목명']), '종목명'] 
+KOR_ticker[str_sub(KOR_ticker[, '종목코드'], -1, -1) != 0, '종목명']
+
+KOR_ticker = KOR_ticker[!grepl('스팩', KOR_ticker[, '종목명']), ]  
+KOR_ticker = KOR_ticker[str_sub(KOR_ticker[, '종목코드'], -1, -1) == 0, ]
+
+#최종 결과 정리 및 저장
+rownames(KOR_ticker) = NULL
+write.csv(KOR_ticker, 'data/KOR_ticker.csv')
