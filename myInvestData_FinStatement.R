58ca114817f4a76269a23f0f7367c0098e0598d2
unknown <seungjae_lee@kolon.com>
2020-03-10 16:35:12 +0900

    드럽게 않되네..아마도 프로시 에러인듯..

---
+++ myInvestData_FinStatement.R
@@ -0,0 +1,22 @@
+#제무제표 다운로드
+#copy in 2020-03-06
+# curl을 인스톨 해야 하나..install.packages(c("curl", "httr"))
+
+
+library(httr)
+library(rvest)
+library(curl)
+
+ifelse(dir.exists('data/KOR_fs'), FALSE,
+       dir.create('data/KOR_fs'))
+
+Sys.setlocale("LC_ALL", "English")
+
+url = paste0('http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A005930')
+
+data = GET(url)
+data = data %>%
+  read_html() %>%
+  html_table()
+
+Sys.setlocale("LC_ALL", "Korean")
